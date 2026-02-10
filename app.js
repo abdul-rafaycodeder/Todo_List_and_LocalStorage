064f8a747044446a807c5d26b057d8a9c73f83c2
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-02-10 08:26:34 -0800

    coding

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log(window.localStorage)
+// console.log(window.localStorage)
 
+const obj = {
+    firsName: 'ali',
+    lastName: 'khan'
+}
+
+var setData = localStorage.setItem('value', obj)
+
+console.log(setData)
