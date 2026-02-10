04dc8c8b5472d588a9d3b038d9f3c4f0987a36a2
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-02-10 09:34:48 -0800

    coding

--- app.js
+++ app.js
@@ -12,10 +12,11 @@
 var key = prompt('enter your kay name');
 var value = prompt('enter your value');
 
-let local = localStorage.setItem(key, value);
+localStorage.setItem(key, value);
 
-console.log(`this is a Key==> ${key} this is a value==> ${localStorage.getItem(key)}`)
 
-if(key == "white" || key == "blue"){
+if (key == "white" || key == "blue"){
     localStorage.removeItem(key)
 }
+
+console.log(`this is a Key==> ${key} this is a value==> ${localStorage.getItem(key)}`)
